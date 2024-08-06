936005c6fecd88ee5f2ae4ef226074176b624e5f
sarkazi2009 <sarkazi2009@gmail.com>
2024-08-06 14:43:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+the_number_of_dz_performed="12"
+TheNumberOfHoursSpent="1.5"
+courseName='Python'
+TimeForOneTask="0.125"
+print("Курс:"+courseName,"всего задач:"+the_number_of_dz_performed,"затрачно часов:"+TheNumberOfHoursSpent,"средне время выполнения:"+TimeForOneTask)
