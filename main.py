3dd1b396a5d24edc41971dc501b45f35dee73acd
sarkazi2009 <sarkazi2009@gmail.com>
2024-08-08 02:23:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-the_number_of_dz_performed="12"
-TheNumberOfHoursSpent="1.5"
-courseName='Python'
-TimeForOneTask="0.125"
-print("Курс:"+courseName,"всего задач:"+the_number_of_dz_performed,"затрачно часов:"+TheNumberOfHoursSpent,"средне время выполнения:"+TimeForOneTask)
+# This is a sample Python script.
+course_name = 'Python'
+home_tasks = 12
+all_hours = 1.5
+time_for_one_task = all_hours/home_tasks
+print('курс:'+course_name+', всего задач:'+str(home_tasks)+', затрачено часов:'+str(all_hours) +
+      ', среднее время выполнения:'+str(time_for_one_task))
